63828d9502cbf9acd8d16134be128f9c31b67646
jel-PDX <james.l_contact@proton.me>
2024-10-25 01:11:22 -0700

    add libcurl boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+#include <curl/curl.h>
+
 #include <iostream>
 
-int main() { std::cout << "stub\n"; }
+int main() {
+  CURL *curl;
+  CURLcode res;
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  curl = curl_easy_init();
+
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL,
+                     "https://cloud-images.ubuntu.com/releases/streams/v1/"
+                     "com.ubuntu.cloud:released:download.json");
+
+    res = curl_easy_perform(curl);
+
+    curl_easy_cleanup(curl);
+  }
+
+  return 0;
+}
